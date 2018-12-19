84f9372b94966be51cd9714ab514b3d08ea51ab5
maivy <vmai2@stanford.edu>
2018-12-19 16:18:01 -0600

    working scroll, remnant non-working spotify code

--- App/Screens/CollectionScreen.js
+++ App/Screens/CollectionScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, FlatList, } from 'react-native';
+import {
+    StyleSheet,
+    View,
+    SafeAreaView,
+    FlatList,
+    Animated, 
+    StatusBar,
+} from 'react-native';
+import { Constants } from 'expo';
 
 import {
     CollectionHeader,
@@ -8,16 +16,21 @@ import {
     CollectionListItem
 } from '../Components';
 
-import { Metrics } from '../Themes';
+import { Metrics, Colors } from '../Themes';
 import PersonalRechords from '../Data/PersonalRechords';
 import FriendRechords from '../Data/FriendRechords';
 import firebase from 'firebase';
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+const HEADER_HEIGHT = Metrics.heights.header + Metrics.heights.sortBy + 65 - 39;
 
 export default class CollectionScreen extends React.Component {  
 
     state = {
         data: [],
-        index: 0
+        index: 0,
+        scrollAnim: new Animated.Value(0),
+        offsetAnim: new Animated.Value(0),
     }
 
     // Function to toggle between personal and friend rechords
@@ -50,6 +63,50 @@ export default class CollectionScreen extends React.Component {
             that.setState({ data: rechords });
         });
     }
+
+    componentDidMount() {
+        this.state.scrollAnim.addListener(this._handleScroll);
+    }
+
+    componentWillUnmount() {
+        this.state.scrollAnim.removeListener(this._handleScroll);
+    }
+
+    _handleScroll = ({ value }) => {
+        this._previousScrollvalue = this._currentScrollValue;
+        this._currentScrollValue = value;
+    };
+
+    _handleScrollEndDrag = () => {
+        // this._scrollEndTimer = setTimeout(this._handleMomentumScrollEnd, 0);
+        this._handleMomentumScrollEnd()
+    };
+
+    _handleMomentumScrollBegin = () => {
+        // clearTimeout(this._scrollEndTimer);
+    };
+
+    _handleMomentumScrollEnd = () => {
+        const currOffset = this.state.offsetAnim.__getValue();
+        if (currOffset === 0) return;
+
+        const previous = this._previousScrollvalue;
+        const current = this._currentScrollValue;
+
+        if (previous > current || current < HEADER_HEIGHT) {
+            // User scrolled down or scroll amount was too less, lets snap back our header
+            Animated.spring(this.state.offsetAnim, {
+                toValue: -current,
+                tension: 300,
+                friction: 35,
+            }).start();
+        } else {
+            Animated.timing(this.state.offsetAnim, {
+                toValue: 0,
+                duration: 500,
+            }).start();
+        }
+    };
 
     getFriendRechords = () => {
         var ref = firebase.database().ref('users').child(firebase.auth().currentUser.uid).child('friendsRechords').orderByChild('date');
@@ -91,60 +148,115 @@ export default class CollectionScreen extends React.Component {
     }
 
     render() {
+        const { scrollAnim, offsetAnim } = this.state;
+
+        const translateY = Animated.add(scrollAnim, offsetAnim).interpolate({
+            inputRange: [0, HEADER_HEIGHT],
+            outputRange: [0, -(HEADER_HEIGHT - 65 + 10)],
+            extrapolate: 'clamp',
+        });
+
+        let toggle =
+            <View style={styles.toggle}>
+                <CollectionToggle
+                    index={this.state.index}
+                    updateIndex={this.updateIndex}
+                />
+            </View>;
+
+        StatusBar.setBarStyle('dark-content', true);
+
         return (
             <SafeAreaView style={styles.container}>
 
-                <CollectionHeader logOut={this.props.screenProps.logOut} />
+            {/* <StatusBar
+                translucent={false}
+                backgroundColor={Colors.white}
+                barStyle="light-content"
+            /> */}
+                {/* <Animated.View style={{ alignItems: 'center', width: '100%' }}>
+                    <CollectionHeader logOut={this.props.screenProps.logOut} />
+
+                    <View style={styles.sortBar}>
+                        <CollectionSortBar />
+                    </View>
+                </Animated.View> */}
+
+                {/* <CollectionHeader logOut={this.props.screenProps.logOut} />
 
                 <View style={styles.sortBar}>
                     <CollectionSortBar />
-                </View>
+                </View> */}
 
-                <View style={styles.toggle}>
+                {/* <View style={styles.toggle}>
                     <CollectionToggle
                         index={this.state.index}
                         updateIndex={this.updateIndex}
                     />
-                </View>
+                </View> */}
 
-                <FlatList
+                {/* <AnimatedScrollView
+                    contentContainerStyle={styles.gallery}
+                    scrollEventThrottle={16}
+                    onScroll={Animated.event([
+                        { nativeEvent: { contentOffset: { y: this.state.scrollAnim } } },
+                    ])}
+                    onMomentumScrollBegin={this._handleMomentumScrollBegin}
+                    onMomentumScrollEnd={this._handleMomentumScrollEnd}
+                    onScrollEndDrag={this._handleScrollEndDrag}>
+                    {PHOTOS.map(uri => (
+                        <Image source={{ uri }} style={styles.photo} />
+                    ))}
+                </AnimatedScrollView> */}
+
+                <Animated.View style={styles.content}>
+                    {/* <View style={styles.toggle}>
+                        <CollectionToggle
+                            index={this.state.index}
+                            updateIndex={this.updateIndex}
+                        />
+                    </View> */}
+
+                    <AnimatedFlatList
+                        contentContainerStyle={styles.list}
+                        columnWrapperStyle={styles.covers}
+                        numColumns={2}
+                        data={this.state.data}
+                        renderItem={({item}) => this.renderRechordItem(item)}
+                        keyExtractor={this._keyExtractor}
+                        // ListHeaderComponent={toggle}
+
+                        scrollEventThrottle={1}
+                        onScroll={Animated.event([
+                            { nativeEvent: { contentOffset: { y: this.state.scrollAnim } } },
+                        ])}
+                        onMomentumScrollBegin={this._handleMomentumScrollBegin}
+                        onMomentumScrollEnd={this._handleMomentumScrollEnd}
+                        onScrollEndDrag={this._handleScrollEndDrag}>
+                    ></AnimatedFlatList>
+                </Animated.View>
+
+                {/* <FlatList
                     columnWrapperStyle={styles.covers}
                     numColumns={2}
                     data={this.state.data}
                     renderItem={({item}) => this.renderRechordItem(item)}
                     keyExtractor={this._keyExtractor}
-                />
+                /> */}
 
-                {/* If we wanna just go with hardcoded data */}
+                <Animated.View style={[styles.hideable, { transform: [{ translateY }] }]}>
+                    <CollectionHeader logOut={this.props.screenProps.logOut} />
 
-                {/* <ScrollView contentContainerStyle={styles.covers}>
-                    <RechordListItem
-                        coverContainerStyle={styles.coverWrapper}
-                        image={Images.cover6}
-                        location='Lynn Canyon'
-                        date='08 31 18'
-                        owner='Tiffany Manuel'
-                        title='Inflatables'
-                    />
+                    <View style={styles.sortBar}>
+                        <CollectionSortBar />
+                    </View>
 
-                    <RechordListItem
-                        coverContainerStyle={styles.coverWrapper}
-                        image={Images.cover2}
-                        location='Del Mar Fairgrounds'
-                        date='06 20 18'
-                        owner='Tiffany Manuel'
-                        title="Del Mar Fair 2018"
-                    />
+                    <View style={styles.toggleView}>
+                        {toggle}
+                    </View>
+                </Animated.View>
 
-                    <RechordListItem
-                        coverContainerStyle={styles.coverWrapper}
-                        image={Images.cover3}
-                        location='Grouse Mountain'
-                        date='08 20 18'
-                        owner='Tiffany Manuel'
-                        title='Jetting Joy'
-                    />
-                </ScrollView> */}
+                <View style={styles.statusBarBackground}></View>
                 
             </SafeAreaView>
         )
@@ -153,21 +265,55 @@ export default class CollectionScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        width: '100%',
         alignItems: 'center',
+        // overflow: 'scroll',
+    },
+    statusBarBackground: {
+        width: '100%',
+        height: Constants.statusBarHeight,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: Colors.white
+    },
+    hideable: {
+        width: '100%',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: Colors.white
+    },
+    content: {
+        width: Metrics.widths.wide,
+        alignItems: 'center',
+        // paddingTop: HEADER_HEIGHT,
         overflow: 'scroll',
+    },
+    list: {
+        width: Metrics.widths.wide,
+        paddingTop: HEADER_HEIGHT,
+        overflow: 'scroll'
     },
     sortBar: {
         marginTop: -39, // move sort bar over header
+        // zIndex: 0,
+    },
+    toggleView: {
+        height: 65,
+        justifyContent: 'center',
     },
     toggle: {
-        marginTop: Metrics.smallMargin,
-        marginBottom: Metrics.smallMargin
+        alignSelf: 'center',
+        // marginTop: Metrics.smallMargin,
+        // marginBottom: Metrics.smallMargin
     },
     covers: {
-        flex: 1,
-        width: Metrics.widths.wide,
-        // flexDirection: 'row',       // For ScrollView Option
-        // flexWrap: 'wrap',
+        // flex: 1,
+        // width: Metrics.widths.wide,
         justifyContent: 'space-between',
     },
     coverWrapper: {
--- App/Screens/NewRechordScreen.js
+++ App/Screens/NewRechordScreen.js
@@ -100,7 +100,18 @@ export default class NewRechordScreen extends React.Component {
         .then((response) => response.json())
         .then((responseJson) => {
             // console.log('ADDRESS GEOCODE is BACK!! => ' + JSON.stringify(responseJson.results[0].address_components[0].short_name));
-            this.setState({ location: responseJson.results[0].address_components[0].short_name } );
+            // this.setState({ location: responseJson.results[0].address_components[0].short_name } );
+            // this.setState({ location: responseJson.results[0].formatted_address } );
+            let address_components = responseJson.results[0].address_components;
+            let street_number = address_components[0].short_name;
+            if (/^\d+$/.test(street_number)) {
+                // this.setState({ location: responseJson.results[0].formatted_address });
+                this.setState({ location: address_components[3].long_name + ', ' + address_components[5].long_name } );
+            } else {
+                // this.setState({ location: address_components[3].long_name + ', ' + address_components[5].long_name } );
+                this.setState({ location: responseJson.results[0].address_components[0].short_name } );
+            }
+
         })
     };
 
